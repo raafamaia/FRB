9be3bf0a1f30965e5d21f94eb92aec29bd7693e7
raafamaia <raafamaia@gmail.com>
2014-03-23 14:48:16 -0300

    Pessoa > Aluno

---
+++ FRBProj/src/model/Aluno.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Aluno extends Pessoa{
+	private int ra;
+	private String periodo;
+	private String curso;
+	public int getRa() {
+		return ra;
+	}
+	public void setRa(int ra) {
+		this.ra = ra;
+	}
+	public String getPeriodo() {
+		return periodo;
+	}
+	public void setPeriodo(String periodo) {
+		this.periodo = periodo;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+	
+}
