cd1f37971cf071d54b5c408dba5b590715f9a301
Victor Sodré <victor.sodres@gmail.com>
2014-05-24 22:19:43 -0300

    Signed-off-by: Victor Sodré <victor.sodres@gmail.com>

---
+++ FRBProj/src/controllers/LoginController.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet(name = "login", urlPatterns = { "/login" })
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+    }
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
