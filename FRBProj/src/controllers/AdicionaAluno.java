5eb7fe38411af51916d013ee83982013604c32c9
Victor Sodré <victor.sodres@gmail.com>
2014-05-24 22:56:49 -0300

    Fix em umas tretas e mudei de index para login.jsp

--- FRBProj/src/controllers/AdicionaAluno.java
+++ FRBProj/src/controllers/AdicionaAluno.java
@@ -1,38 +1,28 @@
 package controllers;
 
-import java.io.IOException;
-
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.Aluno;
 import dataaccess.ListaAlunoDAO;
 
+public class AdicionaAluno {
 
-/**
- * Servlet implementation class AdicionaAluno
- */
-@WebServlet("/adicionaAluno")
-public class AdicionaAluno extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	public void executa(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	public void executa(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException{
 
-//		AlunoDAO alDAO = new AlunoDAO();
+		// AlunoDAO alDAO = new AlunoDAO();
 		ListaAlunoDAO alDao = new ListaAlunoDAO();
-		
+
 		Aluno a = new Aluno();
-		
+
 		a.setNome(request.getParameter("nome"));
 		a.setRa(Integer.parseInt(request.getParameter("RA")));
 		a.setCurso(request.getParameter("curso"));
 		a.setPeriodo(request.getParameter("periodo"));
 		a.setSenha(request.getParameter("senha"));
-		
+
 		alDao.inserir(a);
 	}
 
